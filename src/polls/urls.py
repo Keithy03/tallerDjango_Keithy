474b79f911c93f780f422dec01c82e212c678c67
Keithy Sánchez <sanchezkeithy0@gmail.com>
2024-08-12 16:52:57 -0600

    Taller Parte 4

---
+++ src/polls/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.views.generic import ListView, DetailView
+from . import views
+
+# Se usa para poder diferenciar los nombres de las urls, se recomienda con proyectos muy grandes.
+app_name = "polls"
+
+urlpatterns = [
+    # Las urls para las vistas genericas debido a que las vistas ya creadas (index, detail y results),
+    # son pequeñas y se pueden usar vistas genericas de Django.
+    # Se cambia de question_id a pk.
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+]
